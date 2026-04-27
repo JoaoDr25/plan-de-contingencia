de371911b5cb6d707318f1b8578aeec2bf4f19da
Juan Camilo <jcamilodavila25@gmail.com>
2026-04-27 17:56:27 -0500

    Actualizando controller de programaFormación

--- plan-contingencia-backend/src/controllers/programaFormacionController.js
+++ plan-contingencia-backend/src/controllers/programaFormacionController.js
@@ -2,7 +2,9 @@ import ProgramaFormacion from "../models/programaFormacionModel.js";
 
 export const crearPrograma = async (req, res) => {
     try {
-        
+        const nuevoPrograma = await ProgramaFormacion(req.body);
+        await nuevoPrograma.save();
+        res.status(201).json(nuevoPrograma);
     } catch (error) {
         return res.status(500).json({
             mensaje: "Error al crear el programa de formación",
@@ -13,7 +15,8 @@ export const crearPrograma = async (req, res) => {
 
 export const listarProgramas = async (req, res) => {
     try {
-        
+        const listar = await ProgramaFormacion.find();
+        res.status(201).json(listar);
     } catch (error) {
         return res.status(500).json({
             mensaje: "Error al listar los programas de formación",
@@ -24,7 +27,8 @@ export const listarProgramas = async (req, res) => {
 
 export const obtenerProgramaId = async (req, res) => {
     try {
-        
+        const obtenerId = await ProgramaFormacion.findById(req.params.id);
+        res.status(201).json(obtenerId);
     } catch (error) {
         return res.status(500).json({
             mensaje: "Error al obtener el programa de formación",
@@ -35,7 +39,8 @@ export const obtenerProgramaId = async (req, res) => {
 
 export const actualizarPrograma = async (req, res) => {
     try {
-        
+        const actualizar = await ProgramaFormacion.findByIdAndUpdate(req.params.id);
+        res.status(201).json(actualizar);
     } catch (error) {
         return res.status(500).json({
             mensaje: "Error al actualizar el programa de formación",
@@ -46,7 +51,8 @@ export const actualizarPrograma = async (req, res) => {
 
 export const eliminarPrograma = async (req, res) => {
     try {
-        
+        const eliminar = await ProgramaFormacion.findByIdAndDelete(req.params.id);
+        res.status(201).json(eliminar);
     } catch (error) {
         return res.status(500).json({
             mensaje: "Error al eliminar un programa de formación",
